82b74202a7443057d8e522b1505f93f1d26f1525
Magali Fuentes <magali.fa@gmail.com>
2019-07-31 08:29:03 -0400

    fix unit tests

--- src/test/java/org/fundacionjala/coding/mfuentes/RentMovieTest.java
+++ src/test/java/org/fundacionjala/coding/mfuentes/RentMovieTest.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class RentMovieTest {
-    private final int DAYS_RENTED = 3;
+    private static final int DAYS_RENTED_REGULAR = 4;
+    private static final int DAYS_RENTED_RELEASE = 2;
+    private static final int DAYS_RENTED_CHILDREN = 5;
+
     @Test
     public void movieRentalGetCustomerNameTest() {
         Customer c = new Customer("Angy");
@@ -15,10 +18,10 @@ public class RentMovieTest {
     @Test
     public void movieRentalRegularPrice() {
         Customer customer = new Customer("Maria");
-        customer.addRental(new Rental(new Movie("The King", new RegularPrice()), DAYS_RENTED));
+        customer.addRental(new Rental(new Movie("The King", new RegularPrice()), DAYS_RENTED_REGULAR));
         String expected = "Rental Record for Maria\n"
-                + "\tThe King\t3.5\n"
-                + "Amount owed is 3.5\n"
+                + "\tThe King\t5.0\n"
+                + "Amount owed is 5.0\n"
                 + "You earned 1 frequent renter points";
         String statement = customer.statement();
         assertEquals(expected, statement);
@@ -27,10 +30,10 @@ public class RentMovieTest {
     @Test
     public void movieRentalNewReleasePrice() {
         Customer customer = new Customer("Laura");
-        customer.addRental(new Rental(new Movie("Star Wars", new NewReleasePrice()), DAYS_RENTED));
+        customer.addRental(new Rental(new Movie("Star Wars", new NewReleasePrice()), DAYS_RENTED_RELEASE));
         String expected = "Rental Record for Laura\n"
-                + "\tStar Wars\t9.0\n"
-                + "Amount owed is 9.0\n"
+                + "\tStar Wars\t6.0\n"
+                + "Amount owed is 6.0\n"
                 + "You earned 2 frequent renter points";
         String statement = customer.statement();
         assertEquals(expected, statement);
@@ -39,10 +42,10 @@ public class RentMovieTest {
     @Test
     public void movieRentalChildrenPrice() {
         Customer customer = new Customer("Sofia");
-        customer.addRental(new Rental(new Movie("Madagascar", new ChildrenPrice()), DAYS_RENTED));
+        customer.addRental(new Rental(new Movie("Madagascar", new ChildrenPrice()), DAYS_RENTED_CHILDREN));
         String expected = "Rental Record for Sofia\n"
-                + "\tMadagascar\t1.5\n"
-                + "Amount owed is 1.5\n"
+                + "\tMadagascar\t4.5\n"
+                + "Amount owed is 4.5\n"
                 + "You earned 1 frequent renter points";
         String statement = customer.statement();
         assertEquals(expected, statement);
