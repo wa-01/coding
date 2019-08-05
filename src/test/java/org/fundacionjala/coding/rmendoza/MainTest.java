9a255deef0f2efe002e84038a0ba7acc3268f834
Ruben Mendoza <ruben.mendoza.jala@gmail.com>
2019-08-04 23:06:08 -0400

    Fix Test names

--- src/test/java/org/fundacionjala/coding/rmendoza/MainTest.java
+++ src/test/java/org/fundacionjala/coding/rmendoza/MainTest.java
@@ -5,25 +5,25 @@ import static org.junit.Assert.assertEquals;
 
 public class MainTest {
 
-    final double EXPECTED_TOTAL_CHILDREN_4_DAYS = 3.0;
-    final double EXPECTED_TOTAL_REGULAR_5_DAYS = 6.5;
-    final double EXPECTED_TOTAL_NEW_3_DAYS = 9.0;
-    final double EXPECTED_ONE_OF_EACH_5_DAYS = 26.0;
-    final double EXPECTED_REGULAR_CHILDREN_3_DAYS = 5.0;
+    static final double EXPECTED_TOTAL_CHILDREN_4_DAYS = 3.0;
+    static final double EXPECTED_TOTAL_REGULAR_5_DAYS = 6.5;
+    static final double EXPECTED_TOTAL_NEW_3_DAYS = 9.0;
+    static final double EXPECTED_ONE_OF_EACH_5_DAYS = 26.0;
+    static final double EXPECTED_REGULAR_CHILDREN_3_DAYS = 5.0;
 
-    final int EXPECTED_ONE_OF_EACH_5_DAYS_POINTS = 4;
+    static final int EXPECTED_ONE_OF_EACH_5_DAYS_POINTS = 4;
 
-    private final int RENT_THREE_DAYS = 3;
-    private final int RENT_FOUR_DAYS = 4;
-    private final int RENT_FIVE_DAYS = 5;
-    final int DELTA = 0;
+    static final int RENT_THREE_DAYS = 3;
+    static final int RENT_FOUR_DAYS = 4;
+    static final int RENT_FIVE_DAYS = 5;
+    private final int DELTA_VALUE = 0;
 
     @Test
     public void testChildrenMovieFourDayRental() {
         Statement statement = new Statement("test");
         statement.addRental(new Rental(new MovieChildren("Lion King"), RENT_FOUR_DAYS));
         statement.statement();
-        assertEquals(EXPECTED_TOTAL_CHILDREN_4_DAYS, statement.getTotalStatement(), DELTA);
+        assertEquals(EXPECTED_TOTAL_CHILDREN_4_DAYS, statement.getTotalStatement(), DELTA_VALUE);
     }
 
     @Test
@@ -31,7 +31,7 @@ public class MainTest {
         Statement statement = new Statement("Test");
         statement.addRental(new Rental(new MovieRegular("The Revenant"), RENT_FIVE_DAYS));
         statement.statement();
-        assertEquals(EXPECTED_TOTAL_REGULAR_5_DAYS, statement.getTotalStatement(), DELTA);
+        assertEquals(EXPECTED_TOTAL_REGULAR_5_DAYS, statement.getTotalStatement(), DELTA_VALUE);
     }
 
     @Test
@@ -39,7 +39,7 @@ public class MainTest {
         Statement statement = new Statement("Test");
         statement.addRental(new Rental(new MovieNewRelease("Joker"), RENT_THREE_DAYS));
         statement.statement();
-        assertEquals(EXPECTED_TOTAL_NEW_3_DAYS, statement.getTotalStatement(), DELTA);
+        assertEquals(EXPECTED_TOTAL_NEW_3_DAYS, statement.getTotalStatement(), DELTA_VALUE);
     }
 
     @Test
@@ -49,7 +49,7 @@ public class MainTest {
         statement.addRental(new Rental(new MovieNewRelease("Joker"), RENT_FIVE_DAYS));
         statement.addRental(new Rental(new MovieChildren("Lion King"), RENT_FIVE_DAYS));
         statement.statement();
-        assertEquals(EXPECTED_ONE_OF_EACH_5_DAYS, statement.getTotalStatement(), DELTA);
+        assertEquals(EXPECTED_ONE_OF_EACH_5_DAYS, statement.getTotalStatement(), DELTA_VALUE);
     }
 
     @Test
@@ -58,7 +58,7 @@ public class MainTest {
         statement.addRental(new Rental(new MovieRegular("The Revenant"), RENT_THREE_DAYS));
         statement.addRental(new Rental(new MovieChildren("Lion King"), RENT_THREE_DAYS));
         statement.statement();
-        assertEquals(EXPECTED_REGULAR_CHILDREN_3_DAYS, statement.getTotalStatement(), DELTA);
+        assertEquals(EXPECTED_REGULAR_CHILDREN_3_DAYS, statement.getTotalStatement(), DELTA_VALUE);
     }
 
     @Test
@@ -68,7 +68,7 @@ public class MainTest {
         statement.addRental(new Rental(new MovieNewRelease("Joker"), RENT_THREE_DAYS));
         statement.addRental(new Rental(new MovieChildren("Lion King"), RENT_THREE_DAYS));
         statement.statement();
-        assertEquals(EXPECTED_ONE_OF_EACH_5_DAYS_POINTS, statement.getFrequentRenterPoints(), DELTA);
+        assertEquals(EXPECTED_ONE_OF_EACH_5_DAYS_POINTS, statement.getFrequentRenterPoints(), DELTA_VALUE);
     }
 
     @Test
